7724a17cb3d801bdf6032a3eae72d3384052e3ea
Daniel Qian <danielqian@Daniels-MacBook-Pro-2.local>
2024-01-18 22:17:04 -0800

    Completing preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Opticalc_Prediction
+
+author : Daniel Qian
+
+## About the Project
+
+Working with Eyelabs AI to establish a way to predict IOL Power with regards to Cataract Surgery.
--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import argparse
 import pandas as pd
 import import_ipynb
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-from csvload import df
-
-FULL_COLUMNS = ['Gender', 'Eye', 'Q', 'SA', 'RMS', 'Astig EKR65', 'K1', 'K1 Axis', 'K2', 'K2 Axis', 'Axial Length', 'ACD', 'IOL Power']
-PREDICTION_COLUMN = ['Outcome SE/refraction\n(after surgery vision)']
-
-full_column_df = df[FULL_COLUMNS]
-no_na_row_df = df.dropna()
+from csvload import FULL_COLUMNS, df, train, test, all_column_train, all_column_y, all_column_test, all_columns_test_result, no_na_train, no_na_train_y, no_na_test, no_na_test_y
 
 def MLR(args):
+    regr = LinearRegression()
+    if args.model_type == "Full":
+        data = all_column_train[FULL_COLUMNS]
+        y_data = all_column_y
+        test_data = all_column_test[FULL_COLUMNS]
+        test_results = all_columns_test_result
+    else:
+        data = no_na_train
+        y_data = no_na_train_y
+        test_data = no_na_test
+        test_results = no_na_test_y
+    
+    regr.fit(data, y_data)
+    
     pass
 
 def logistic(args):
     pass
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="Script for Eyelabs AI")
-    # parser.add_argument("-csvf", "--csv_file", required=True, help="enter name of csv file to load")
-    # args = parser.parse_args()
-    # main(args)
-    print(full_column_df.columns)
+    parser = argparse.ArgumentParser(description="Script for Eyelabs AI")
+    parser.add_argument("-tech", "--technique", required=True, help="enter technique to run predictions (MLR, Log")
+    parser.add_argument("-mt", "--model_type", required=True, help="Full or All")
+    args = parser.parse_args()
